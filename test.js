e69cee383603e76b6cd44dfb8abf106b23fddbf8
David <david@outlier.org>
2019-06-14 12:26:08 -0700

    feat: add base64 endpoint

--- test.js
+++ test.js
@@ -9,18 +9,25 @@ const urlBase = `http://localhost:${PORT}`
 tape('should respond hello', (t) => {
   jsonist.get(urlBase, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.msg, 'hello')
     t.end()
   })
 })
 
+
 tape('should respond user-agent', (t) => {
   const opts = { headers: { 'User-Agent': 'tape' } }
   jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
     if (err) t.error(err)
-
     t.equal(body.ua, 'tape')
+    t.end()
+  })
+})
+
+tape('should respond b64', (t) => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err)
+    t.equal(body.b64, 'aGVsbG8=')
     t.end()
   })
 })
